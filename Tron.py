36f3b8be0da2c541d1fc77e6683cdeb92afd5e2c
Brian Bowers <bmbowers22@gmail.com>
2022-11-26 19:38:04 -0800

    drawing stuff

--- Tron.py
+++ Tron.py
@@ -9,29 +9,46 @@ class snake:
     pass
 
 class game:
+
     def __init__(self, height, width):
         self.height = height
-        self.width = width
+        self.width = width   
 
     def create_screen(self):
         screen = pygame.display.set_mode([self.height, self.width])
+        pygame.display.set_caption('Tron') 
+        screen.fill((255,255,255))
+        return screen
         
     def add_background(self, image):
         background = pygame.image.load(image)
         #back_rect = background.get_rect()
+
+    def draw_grid(self, screen):
+        x_value = 0
+        y_value = 0
+        for x in range(20):
+            for y in range(20):
+                pygame.draw.rect(screen, (0,0,0), [x_value, y_value, self.width/20, self.height/20], 3)
+                y_value += self.height/20
+            x_value += self.width/20
+            y_value = 0
     
-    def start(self):
-        'This keeps the screen displaying until the screen windown is closed'
+    def start_play(self):
+
+        screen = self.create_screen()
+        self.draw_grid(screen)
+        pygame.display.update()    
+
+        'This keeps the screen displaying until the screen window is closed'
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: sys.exit()
 
-            self.create_screen()
-            self.add_background()
             
 
 class player:
     pass
 
 x = game(400,400)
-x.start()
+x.start_play()
