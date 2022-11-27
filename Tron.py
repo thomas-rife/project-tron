80a20650f7f2ef46b14073a5a13b92a487ed2871
Brian Bowers <bmbowers22@gmail.com>
2022-11-26 20:40:26 -0800

    Drawing squares

--- Tron.py
+++ Tron.py
@@ -6,17 +6,39 @@ import sys, pygame
 pygame.init()
 
 class snake:
-    def __init__(self, screen, color):
+    def __init__(self, screen, color, left_player):
         self.screen = screen
         self.color = color
+        self.left_player = left_player
+        self.current_x = 0
+        self.current_y = 0
+
+    def get_size(self):
+        size = pygame.display.get_surface().get_size()
+        return size
 
     def draw_snake(self):
-        start_x = self.screen.width *2
-        start_y = self.screen.height/2
-        pygame.draw.rect(self.screen, self.color, [start_x, start_y, self.width/20, self.height/20])
+        x,y = self.get_size()
+        if self.left_player:
+            start_x = x/20 *2
+        else:
+            start_x = x - (x/20 *3)
+        self.current_x = start_x
+        start_y = y/2
+        self.current_y = start_y
+        pygame.draw.rect(self.screen, self.color, [start_x, start_y, x/20, y/20])
     
     def extend_snake(self, direction):
-        pass
+        'direction is in relation to the screen, not in regard to the snake'
+        x,y = self.get_size()
+        directions = {'up': -(y/20), 'down': (y/20), 'left': -(x/20), 'right': x/20}
+        if direction == 'left' or direction == 'right':
+            pygame.draw.rect(self.screen, self.color, [self.current_x + directions[direction], self.current_y, x/20, y/20])
+            self.current_x += directions[direction]
+        else:
+            pygame.draw.rect(self.screen, self.color, [self.current_x, self.current_y + directions[direction], x/20, y/20])
+            self.current_y += directions[direction]
+
 
 class game:
 
@@ -50,8 +72,20 @@ class game:
         self.draw_grid(screen)
         pygame.display.update() 
 
-        snake1 = snake(screen, ())   
+        snake1 = snake(screen, (255, 0, 0), True)
+        snake1.draw_snake() 
+        snake2 = snake(screen, (0,0,255), False) 
+        snake2.draw_snake()
+        pygame.display.update() 
 
+
+        snake1.extend_snake('right')
+        snake2.extend_snake('up')
+        snake1.extend_snake("right")
+        snake1.extend_snake("right")
+        snake2.extend_snake('left')
+        snake2.extend_snake('down')
+        pygame.display.update()
         'This keeps the screen displaying until the screen window is closed'
         while True:
             for event in pygame.event.get():
