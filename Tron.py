7e9251e83fe6fd5d2dc70d735966b0e04b1d261e
Brian Bowers <bmbowers22@gmail.com>
2022-11-29 15:14:10 -0800

    Collision

--- Tron.py
+++ Tron.py
@@ -6,12 +6,13 @@ import sys, pygame
 pygame.init()
 
 class snake:
-    def __init__(self, screen, color, left_player):
+    def __init__(self, screen, color, left_player, moves):
         self.screen = screen
         self.color = color
         self.left_player = left_player
         self.current_x = 0
         self.current_y = 0
+        self.moves = moves
 
     def get_size(self):
         size = pygame.display.get_surface().get_size()
@@ -27,18 +28,24 @@ class snake:
         start_y = y/2
         self.current_y = start_y
         pygame.draw.rect(self.screen, self.color, [start_x, start_y, x/20, y/20])
+        self.moves.append((self.current_x, self.current_y))
     
     def extend_snake(self, direction):
+        print('this function is being called')
         'direction is in relation to the screen, not in regard to the snake'
         x,y = self.get_size()
         directions = {'up': -(y/20), 'down': (y/20), 'left': -(x/20), 'right': x/20}
         if direction == 'left' or direction == 'right':
+            print("not drawing over the fill")
             pygame.draw.rect(self.screen, self.color, [self.current_x + directions[direction], self.current_y, x/20, y/20])
             self.current_x += directions[direction]
         else:
             pygame.draw.rect(self.screen, self.color, [self.current_x, self.current_y + directions[direction], x/20, y/20])
             self.current_y += directions[direction]
-
+        if (self.current_x, self.current_y) in self.moves or self.current_x <0 or self.current_x > (x - x/20) or self.current_y < 0 or self.current_y > (y - y/20):
+            return True
+        self.moves.append((self.current_x, self.current_y))
+        
 
 class game:
 
@@ -46,6 +53,7 @@ class game:
         self.height = height
         self.width = width   
         self.difficulty = difficulty
+        self.moves = []
 
     def create_screen(self):
         screen = pygame.display.set_mode([self.height, self.width])
@@ -66,38 +74,46 @@ class game:
                 y_value += self.height/20
             x_value += self.width/20
             y_value = 0
+    '''
+    def new_screen(self, screen):
+        
+        
+        return (snake1, snake2)
+        '''
     
     def start_play(self):
-
-        screen = self.create_screen()
-        self.draw_grid(screen)
-        pygame.display.update() 
-
-        snake1 = snake(screen, (255, 0, 0), True)
-        snake1.draw_snake() 
-        snake2 = snake(screen, (0,0,255), False) 
-        snake2.draw_snake()
-        pygame.display.update() 
+        for x in range(3):
+            screen = self.create_screen()
+            self.moves = []
+            screen.fill((255,255,255))
+            self.draw_grid(screen)
+            snake1 = snake(screen, (255, 0, 0), True, self.moves)
+            snake1.draw_snake() 
+            snake2 = snake(screen, (0,0,255), False, self.moves) 
+            snake2.draw_snake()
+            pygame.display.update() 
 
         'This keeps the screen displaying until the screen window is closed'
         player1 = player(0, 'letters',snake1, 'right')
         player2 = player(0, 'arrows', snake2, 'left')
         while True:
-            direction = player1.dir
-            direction2 = player2.dir
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT: sys.exit()
+                direction = player1.dir
+                direction2 = player2.dir
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT: sys.exit()
                 
-                if event.type == pygame.KEYDOWN:
+                    if event.type == pygame.KEYDOWN:
                 #and (event.key == pygame.K_a or event.key == pygame.K_d or event.key == pygame.K_w or event.key == pygame.K_s or event.key == pygame.K_UP or event.key == pygame.K_DOWN or event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT):
-                    direction = player1.player_input(event)
-                    direction2 = (player2.player_input(event))
+                        direction = player1.player_input(event)
+                        direction2 = (player2.player_input(event))
 
-            snake1.extend_snake(direction)
-            snake2.extend_snake(direction2)
-            pygame.display.update()
-            pygame.time.wait(self.difficulty)
-            
+                if snake1.extend_snake(direction):
+                    break
+                elif snake2.extend_snake(direction2):
+                    break
+                else:    
+                    pygame.display.update()
+                    pygame.time.wait(self.difficulty)
             
 
 class player:
