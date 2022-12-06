57e072797ad3a3c80fc0af90e2c170f3d2228b7b
robcas23 <robert_castro@Roberts-MacBook-Pro.local>
2022-12-05 17:28:17 -0800

    countdown

--- Tron.py
+++ Tron.py
@@ -3,9 +3,8 @@ The tron game.
 Brian, Thomas, Robert
 """
 import sys, pygame
-import menu
 pygame.init()
-
+import time
 class snake:
     def __init__(self, screen, color, left_player):
         self.screen = screen
@@ -31,13 +30,12 @@ class snake:
         pygame.draw.rect(self.screen, self.color, [start_x, start_y, x/20, y/20])
         moves.append((self.current_x, self.current_y))
     
-    def extend_snake(self, direction):
-        print('this function is being called')
+    #draws a square in the given direction and checks to see if the snake's move is illegal to which it will return true.
+    def extend_snake(self, direction, moves):
         'direction is in relation to the screen, not in regard to the snake'
         x,y = self.get_size()
         directions = {'up': -(y/20), 'down': (y/20), 'left': -(x/20), 'right': x/20}
         if direction == 'left' or direction == 'right':
-            print("not drawing over the fill")
             pygame.draw.rect(self.screen, self.color, [self.current_x + directions[direction], self.current_y, x/20, y/20])
             self.current_x += directions[direction]
         else:
@@ -73,45 +71,77 @@ class game:
                 y_value += self.height/20
             x_value += self.width/20
             y_value = 0
-    '''
-    def new_screen(self, screen):
+
+    def new_round(self, snake1, snake2, screen, player1, player2):
+        self.moves = []
+                    
+        screen.fill((255,255,255))
+        self.draw_grid(screen)
+        player1.dir = 'right'
+        player2.dir = 'left'
+        snake1.draw_snake(self.moves) 
+        snake2.draw_snake(self.moves)
+        # self.countdown(screen)
+        pygame.display.update()
+
+    def winner(self, screen, winner, color):
+        font = pygame.font.SysFont("arialblack",75)
+        img = font.render(winner + ' WINS!',True, color)
+        screen.blit(img,(15, 125))
+
+    #this function starts a countdown from 3 at the start of each round
+    def countdown(self, screen):
+       for x in range(3, 0, -1):
+        screen.fill((255,255,255))
+        self.draw_grid(screen)
+        font = pygame.font.SysFont("arialblack",75)
+        img = font.render(str(int(x)), True, 'Blue')
+        screen.blit(img,(175, 125))
+        pygame.time.delay(1000)
+        pygame.display.update()
+       screen.fill((255,255,255))
+       self.draw_grid(screen)
+        # print(x)
+    #     text_color = (255,171,10)
         
-        
-        return (snake1, snake2)
-        '''
+                
     
+    #this function initilizes the snakes and the players and starts the game
     def start_play(self):
         
-        for x in range(3):
+        #for x in range(3):
             screen = self.create_screen()
             snake1 = snake(screen, (255, 16, 240), True)
             snake2 = snake(screen, (255,234,0), False)
             player1 = player('letters',snake1, 'right')
             player2 = player('arrows', snake2, 'left')
             
+            self.countdown(screen)
+            pygame.time.delay(3000)
             self.new_round(snake1, snake2, screen, player1, player2)
             
 
             "add a countdown function so its not right away"
 
             'This keeps the screen displaying until the screen window is closed'
-            player1 = player(0, 'letters',snake1, 'right')
-            player2 = player(0, 'arrows', snake2, 'left')
-            while True:
-                direction = player1.dir
-                direction2 = player2.dir
+            #this is the loop that allows for the snake to look like its moving
+            while player1.score < 3 and player2.score < 3:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT: sys.exit()
                 
                     if event.type == pygame.KEYDOWN:
-                #and (event.key == pygame.K_a or event.key == pygame.K_d or event.key == pygame.K_w or event.key == pygame.K_s or event.key == pygame.K_UP or event.key == pygame.K_DOWN or event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT):
-                        direction = player1.player_input(event)
-                        direction2 = (player2.player_input(event))
+                        player1.player_input(event)
+                        player2.player_input(event)
+                pygame.display.update()
+                if snake1.extend_snake(player1.dir, self.moves):
+                    player2.add_point()
+                    self.new_round(snake1, snake2, screen, player1, player2)
+                    pygame.time.delay(500)
 
-                if snake1.extend_snake(direction):
-                    break
-                elif snake2.extend_snake(direction2):
-                    break
+                elif snake2.extend_snake(player2.dir, self.moves):
+                    player1.add_point()  
+                    self.new_round(snake1, snake2, screen, player1, player2)
+                    pygame.time.delay(500)
                 else:    
                     pygame.display.update()
                     pygame.time.wait(self.difficulty)
@@ -136,20 +166,32 @@ class player:
         
     #this class takes in the event and checks to see if the move is allowed (not in the opposite direction) and returns the direction to move
     def player_input(self, event):
+        anti_direct = {'left':'right', 'right':'left', 'up':'down', 'down':'up'}
         if self.keys == 'letters':
             input_letters = {pygame.K_a : 'left', pygame.K_s : 'down',pygame.K_d : 'right',pygame.K_w : 'up'}
             if event.key in input_letters:
-                self.dir = input_letters[event.key]
+                  direct = input_letters[event.key]
+                  if direct != anti_direct[self.dir]:
+                    self.dir = direct
 
         elif self.keys == 'arrows':
             input_arrows = {pygame.K_UP: 'up', pygame.K_DOWN: 'down' , pygame.K_LEFT: 'left', pygame.K_RIGHT: 'right'} 
             if event.key in input_arrows:
-                self.dir = input_arrows[event.key]
+                direct = input_arrows[event.key]
+                if direct != anti_direct[self.dir]:
+                    self.dir = direct
 
         return self.dir
 
-
-
-
-x = game(400,400, 200)
-x.start_play()
+    def add_point(self):
+        self.score += 1
+    
+    def update(self, screen, txt_color, position):
+        #draws the outline in black
+        font = pygame.font.SysFont("arialblack",55)
+        img = font.render(str(self.score),True,(0,0,0))
+        screen.blit(img,position)
+        #draws the color of the snake
+        font = pygame.font.SysFont("arialblack",50)
+        img = font.render(str(self.score),True,txt_color)
+        screen.blit(img,position)
